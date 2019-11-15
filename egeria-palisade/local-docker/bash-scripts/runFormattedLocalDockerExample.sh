389aa49d0387b2b27fa07cbe5776b6e7f445b2fd
developer01189998819991197253 <ChrisCoDev@gmail.com>
2019-11-15 14:33:14 +0000

    updated set-env and modified dockerfile

--- egeria-palisade/local-docker/bash-scripts/runFormattedLocalDockerExample.sh
+++ egeria-palisade/local-docker/bash-scripts/runFormattedLocalDockerExample.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-. "$DIR/../../bash-scripts/setScriptPath.sh"
+. "$DIR/setScriptPath.sh"
 
 # Check that the required network is present
 NETWORKS=$(docker network ls -f 'name=example_palisade' --format '{{.Name}}')
@@ -10,5 +10,5 @@ if [[ $(grep -c example_palisade <(echo $NETWORKS)) -lt 1 ]]; then
     exit 1
 fi
 
-docker build -t example-docker-runner "$EXAMPLE/deployment/local-docker/example-docker-services/client/"
+docker build -t example-docker-runner "$EXAMPLE/local-docker/example-docker-services/client/"
 docker run --network=example_palisade_network --rm example-docker-runner | $EXAMPLE/deployment/bash-scripts/formatOutput.sh
