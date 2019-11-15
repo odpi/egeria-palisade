f1e4ea448405c56dc83f9625e977a1eea245876d
developer01189998819991197253 <ChrisCoDev@gmail.com>
2019-11-15 15:00:59 +0000

    renamed example-docker-services in files to example

--- egeria-palisade/local-docker/bash-scripts/dockerComposeCreateOnly.sh
+++ egeria-palisade/local-docker/bash-scripts/dockerComposeCreateOnly.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . "$DIR/setScriptPath.sh"
-docker-compose --no-ansi -f "$EXAMPLE/local-docker/example-docker-services/docker-compose.yml" -p example create --build
+docker-compose --no-ansi -f "$EXAMPLE/local-docker/example/docker-compose.yml" -p example create --build
