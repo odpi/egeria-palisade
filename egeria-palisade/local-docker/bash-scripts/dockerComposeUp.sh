848f5f89f0ba1acfa051bcfeeb46241a941d6035
developer01189998819991197253 <ChrisCoDev@gmail.com>
2019-11-15 13:33:26 +0000

    removed setEnv

--- egeria-palisade/local-docker/bash-scripts/dockerComposeUp.sh
+++ egeria-palisade/local-docker/bash-scripts/dockerComposeUp.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
-set -e
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-. "$DIR/../../bash-scripts/setScriptPath.sh"
-docker-compose --no-ansi -f "$EXAMPLE/deployment/local-docker/example-docker-services/docker-compose.yml" -p example up -d --build
+docker-compose --no-ansi -f "../example-docker-services/docker-compose.yml" -p example up -d --build
