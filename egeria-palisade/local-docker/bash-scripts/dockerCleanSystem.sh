57fdc9fe898b617b470094d880786b20d3f0ea5f
developer01189998819991197253 <ChrisCoDev@gmail.com>
2019-11-15 13:33:26 +0000

    removed setEnv
    Signed-off-by: developer01189998819991197253 <ChrisCoDev@gmail.com>

--- egeria-palisade/local-docker/bash-scripts/dockerCleanSystem.sh
+++ egeria-palisade/local-docker/bash-scripts/dockerCleanSystem.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-set -e
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-. "$DIR/../../bash-scripts/setScriptPath.sh"
-"$DOCKERBASHSCRIPTS/dockerComposeDown.sh"
+"./dockerComposeDown.sh"
 docker system prune -a
