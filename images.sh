b5a1334f0051edb7172a0587de6a6e764327d191
pd104923 <54805408+pd104923@users.noreply.github.com>
2019-11-29 11:10:16 +0000

    Added jupyter Dockerfile so example-model jar, updated images script to build
    Signed-off-by: pd104923 <54805408+pd104923@users.noreply.github.com>

--- images.sh
+++ images.sh
@@ -3,3 +3,6 @@ set -e
 
 # Build docker images for palisade and store them to the image repo
 (cd palisade/local-docker/example && docker-compose -f docker-compose.yml build)
+
+# Build local docker image for Jupyter
+(cd palisade && docker build . -t jupyter/base-notebook:latest -f local-docker/example/jupyter/Dockerfile)
