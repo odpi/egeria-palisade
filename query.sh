e28a9c9bfbd3bc58255a50b0bcd7c3c10cab6acf
ac74475 <ac74475@users.noreply.github.com>
2019-12-03 17:35:11 +0000

    fixed a bug in query script
    Signed-off-by: ac74475 <ac74475@users.noreply.github.com>

--- query.sh
+++ query.sh
@@ -2,4 +2,4 @@
 
 export PALISADE_REST_CONFIG_PATH="/home/jovyan/resources/configRest.json"
 
-java -cp /home/jovyan/resources/example-model-*-shaded.jar uk.gov.gchq.palisade.example.client.ExampleSimpleClient $1 $2 $3
+java -cp /home/jovyan/resources/example-model-*-shaded.jar uk.gov.gchq.palisade.example.client.ExampleSimpleClient $0 $1 $2
