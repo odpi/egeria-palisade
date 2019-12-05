4aff43c5ec0480fd1c6191b167597a9a00daf6f0
ac74475 <ac74475@users.noreply.github.com>
2019-12-05 13:04:40 +0000

    updated the install script and readme to show which commit to checkout for Palisade (#22)
    Signed-off-by: ac74475 <ac74475@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 cd ..
 
 git clone https://github.com/gchq/Palisade.git
-(cd palisade && git checkout develop)
+(cd palisade && git checkout 4f408c9a3e069dad4222b4a8ee6fa5ea2d174308)
 
 git clone https://github.com/odpi/egeria.git
 (cd egeria && git checkout 2f8e861a7204c4bc00ec466fecd3ddf0820168e6)
